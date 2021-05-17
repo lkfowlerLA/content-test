0c331a072062c6f212ffc28286f575700ee15c5d
Landon Fowler <landon.fowler@linuxacademy.com>
2021-05-17 10:55:42 -0400

    python scripts

--- aml/data_avg.py
+++ aml/data_avg.py
@@ -1,4 +1,24 @@
+# Import numpy
 import numpy as np
 
-temps = np.array(data)
-avg_temp = temps.average()
+# Get the temperature list from the data file
+with open('datafile.txt') as fp:
+  data = fp.read()
+
+# Remove brackets from the list
+data = data.strip('[')
+data = data.strip(']')
+
+# Split the values by comma and convert to int
+temps = data.split(',')
+temps = [int(i) for i in temps]
+
+# Make the list an array
+tempArray = np.array(temps)
+
+# Average the values
+avg = np.average(tempArray)
+
+# Write the average out to a file
+file2 = open('average.txt', 'w')
+file2.writelines(str(avg))
--- aml/data_prep.py
+++ aml/data_prep.py
@@ -1,1 +1,6 @@
+# Create a series of temperature values
 data = [49,60,77,32,55,90,28,60,83,75,62,18,85,72,58,67,42,33,98,70]
+
+# Write the list to a file
+file1 = open('datafile.txt', 'w')
+file1.writelines(str(data))
